d2460de4ee90280097b15ed679aa5ffea9e98219
Diego Garcia <diego.g@onthegosystems.com>
2026-03-04 09:46:48 +0100

    wpmldev-4407: PHP 8.4 compatibility

--- src/Illuminate/Support/Arr.php
+++ src/Illuminate/Support/Arr.php
@@ -131,7 +131,7 @@ class Arr
      * @param  mixed  $default
      * @return mixed
      */
-    public static function first($array, $callback = null, $default = null)
+    public static function first($array, ?callable $callback = null, $default = null)
     {
         if (is_null($callback)) {
             if (empty($array)) {
@@ -160,7 +160,7 @@ class Arr
      * @param  mixed  $default
      * @return mixed
      */
-    public static function last($array, $callback = null, $default = null)
+    public static function last($array, ?callable $callback = null, $default = null)
     {
         if (is_null($callback)) {
             return empty($array) ? value($default) : end($array);
